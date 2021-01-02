5cca862ca989036b47ae38161732e0f039dc1166
Enis Getmez <mentalistler@mit.tc>
2021-01-02 23:27:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+  'use strict';
+
+  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Enis Getmez.", "a Developer.", "an Entrepreneur.", "a Dreamer."],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+
+
+
+
+});
